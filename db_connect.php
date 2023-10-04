e8cf23ee27fb8df69f2662b324947b0f2a96457e
devadethanr <125908755+devadethanr@users.noreply.github.com>
2023-10-04 20:56:36 +0530

    db schema added

--- db_connect.php
+++ db_connect.php
@@ -1,4 +1,4 @@
 <?php
-    $conn = mysqli_connect("opensynth.cf5avj80tekl.eu-north-1.rds.amazonaws.com","admin","OOoo1212","opensynth")
+    $conn = mysqli_connect("opensynth.cquy1dijuc9e.ap-south-1.rds.amazonaws.com","admin","OOoo1212","opensynth_db")
     or die("db not connected");
 ?>
