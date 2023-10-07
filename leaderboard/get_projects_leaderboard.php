765d429995bc755033fbee447296edd51242b690
devadethanr <125908755+devadethanr@users.noreply.github.com>
2023-10-07 18:16:42 +0530

    leaderboard

---
+++ leaderboard/get_projects_leaderboard.php
@@ -0,0 +1,39 @@
+<?php
+// Establish a MySQL database connection here if not already established
+// Include database connection code here
+
+// MySQL Query for Ranking Projects
+$query = "
+    SELECT
+        p.Title AS Name,
+        AVG(pr.Rating) AS Score
+    FROM
+        Projects p
+    LEFT JOIN
+        ProjectRatings pr ON p.ProjectID = pr.ProjectID
+    GROUP BY
+        p.ProjectID, p.Title
+    ORDER BY
+        Score DESC
+";
+
+$result = mysqli_query($connection, $query);
+
+$data = array();
+
+if ($result) {
+    $rank = 1;
+    while ($row = mysqli_fetch_assoc($result)) {
+        $data[] = array(
+            'Rank' => $rank,
+            'Name' => htmlspecialchars($row['Name']),
+            'Score' => htmlspecialchars($row['Score'])
+        );
+        $rank++;
+    }
+} else {
+    $data[] = array("Rank" => "Error fetching data", "Name" => "", "Score" => "");
+}
+
+echo json_encode(array('data' => $data));
+?>
